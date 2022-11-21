79c711ed5b604cced5fa994d78491e8a2ef7d932
Varun Joseph <63628481+varun-joseph@users.noreply.github.com>
2022-11-21 20:34:08 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,13 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://pizzaapplication-mern.herokuapp.com/' })
 
-API.interceptors.request.use((req) => {
-   if (localStorage.getItem('profile')) {
-      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
-   }
-
-   return req;
-})
 
 export const registerUser=(user)=>async dispatch=>{
 
